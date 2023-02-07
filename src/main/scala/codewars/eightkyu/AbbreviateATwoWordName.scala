0752310327f565b23f81cde3d19d5abd8b0e899c
ZiynatI <zisomurodova@gmail.com>
2023-02-07 15:23:06 +0500

    AbbreviateATwoWordName.scala created

---
+++ src/main/scala/codewars/eightkyu/AbbreviateATwoWordName.scala
@@ -0,0 +1,14 @@
+package codewars.eightkyu
+
+object AbbreviateATwoWordName {
+  //https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3
+  //Write a function to convert a name into initials.
+  //This kata strictly takes two words with one space in between them.
+  //The output should be two capital letters with a dot separating them.
+  //It should look like this:
+  //Sam Harris => S.H
+  //patrick feeney => P.F
+  def abbrevName(name: String): String = {
+    name.charAt(0).toUpper + "" + name.charAt(name.indexOf(' ') + 1).toUpper
+  }
+}
