1dec21bdd8b8248d134e58cb5317656f67ffcdf6
ZiynatI <zisomurodova@gmail.com>
2023-02-18 20:09:30 +0500

    SubtractTheSum.scala updated

--- src/main/scala/codewars/eightkyu/SubtractTheSum.scala
+++ src/main/scala/codewars/eightkyu/SubtractTheSum.scala
@@ -19,6 +19,7 @@ object SubtractTheSum {
   //.
   //...until you find the first n in the list below.
   //There is no preloaded code to help you. This is not about coding skills; think before you code
+
   def subtractSum(n: Int): String = {
     val mapString =
       """1-kiwi
